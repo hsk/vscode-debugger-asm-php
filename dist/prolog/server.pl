5731d50f3f522c7fabb7a2fcfd9a78a8385703f8
hsk <sakurai777@gmail.com>
2018-12-22 18:40:18 +0900

    added setVariable setting

--- dist/prolog/server.pl
+++ dist/prolog/server.pl
@@ -1,15 +1,10 @@
 #!/usr/bin/env swipl
-:- use_module(library(http/json)).
-:- use_module(parser).
-:- use_module(vm).
+:- use_module(library(http/json)), use_module(parser), use_module(vm).
 
 % DebugSession
 
-:- nb_setval(lineAt1, true).
-:- nb_setval(columnAt1, true).
-:- nb_setval(seq, 1).
-:- nb_setval(last_request_seq, 0).
-:- dynamic(breakPoints/2).
+:- nb_setval(lineAt1, true), nb_setval(columnAt1, true), nb_setval(seq, 1).
+:- nb_setval(last_request_seq, 0), dynamic(breakPoints/2).
 
 setDebuggerLinesStartAt1(Bool) :- nb_setval(lineAt1, Bool).
 setDebuggerColumnsStartAt1(Bool) :- nb_setval(columnAt1, Bool).
@@ -17,24 +12,22 @@ convertClientLineToDebugger(I, I) :- nb_getval(lineAt1, true), !.
 convertClientLineToDebugger(I, I1) :- nb_getval(lineAt1,false),!,I1 is I-1.
 convertDebuggerLineToClient(I, I) :- nb_getval(lineAt1, true), !.
 convertDebuggerLineToClient(I, I1) :- nb_getval(lineAt1,false),!,I1 is I+1.
+convertDebuggerPathToClient(P,P).
 
 get_seq(Seq) :- nb_getval(seq, Seq),Seq_ is Seq+1,nb_setval(seq, Seq_), !.
 sendJSON(Arr) :-
   get_seq(Seq),atom_json_dict(Str, Arr.put(seq,Seq), [width(0)]), !,
   log1('<~w\r\n', [Str]),atom_length(Str, L),
   format('Content-Length: ~d\r\n\r\n~s', [L, Str]),flush_output.
-%:- sendJSON(json([a=a])).
 sendEvent(Event) :- sendJSON(_{type:event, event:Event}).
 sendEvent(Event, Body) :- sendJSON(_{type:event, event:Event, body:Body}).
-
 sendResponse(Arr) :- nb_getval(last_request_seq, S),Arr.get(request_seq)=S, !.
-sendResponse(Arr) :-
-    nb_getval(last_request_seq, S),S_ is S+1,nb_setval(last_request_seq, S_), !,
-    sendJSON(Arr).
-log(Msg) :-
-    atomic_list_concat(Msg,Msg1),
-    atom_concat(Msg1, '\n', Msg_),
-    sendEvent(output, _{category:console, output:Msg_}).
+sendResponse(Arr) :- nb_getval(last_request_seq, S),S_ is S+1,
+                     nb_setval(last_request_seq, S_),!,sendJSON(Arr).
+sendTerminated(Res) :- sendResponse(Res), sendEvent(terminated).
+
+log(Msg) :- atomic_list_concat(Msg,Msg1),atom_concat(Msg1, '\n', Msg_),
+            sendEvent(output, _{category:console, output:Msg_}).
 log1(L, Ls) :- format(user_error, L, Ls), !.
 log1(L) :- writeln(user_error, L), !.
 log2(Str) :- open('/tmp/recive.log', append, Handle),
@@ -47,15 +40,14 @@ trim(A,A).
 
 readData(R) :- readData(0,R).
 readData(Len,R) :-
-    prompt1(''),
-    \+ at_end_of_stream,
-    read_line_to_codes(user_input,IN1),atom_codes(IN,IN1),
-    trim(IN,IN_),!,
-    (IN_='',!,readData2(Len,R)
-    ;atom_concat('Content-Length: ',Len2,IN_),!,readData(Len2,R)
-    ;readData(Len,R)).
+  prompt1(''), \+ at_end_of_stream,
+  read_line_to_codes(user_input,IN1),atom_codes(IN,IN1),trim(IN,IN_),!,
+  (IN_='',!,readData2(Len,R)
+  ;atom_concat('Content-Length: ',Len2,IN_),!,readData(Len2,R)
+  ;readData(Len,R)).
 readData2(0,_) :- !,false.
-readData2(Len,R) :- !,atom_number(Len,Len2), prompt1(''),read_string(user_input,Len2,Str),!,atom_string(R,Str),!.
+readData2(Len,R) :- !,atom_number(Len,Len2), prompt1(''),
+                    read_string(user_input,Len2,Str),!,atom_string(R,Str),!.
 
 % Main
 
@@ -65,23 +57,24 @@ dispatch_loop :- readData(Data),dispatch(Data),dispatch_loop.
 dispatch_loop.
 dispatch(Data) :-
     log2(Data),log1(Data),
-    trim(Data,Data_),atom_json_dict(Data_,Data2,[value_string_as(atom)]),writeln(Data2),
+    trim(Data,Data_),atom_json_dict(Data_,Data2,[value_string_as(atom)]),
     Command = Data2.command,
-    writeln(command:Command),
     (\+ current_predicate(command:Command/2) -> log(['unknown command ',Data_])
     ; Res= _{type:response,request_seq:Data2.get(seq),
              command:Data2.command,success:true},
       (Res2 = Res.put(body,Data2.get(body));Res2=Res),
       (Args=Data2.get(arguments); Args=null),
-      writeln(args:Args),
       nb_setval(last_request_seq,0),
       call(command:Command,Res2,Args),
       log1('call method ok-------------------'),
       (Command=disconnect, halt;true)
     ).
 
+% 例外時ブレークポイント
 command:setExceptionBreakpoints(Res, _) :- sendResponse(Res).
+% 設定終了
 command:configurationDone(Res, _) :- sendResponse(Res).
+% 接続終了
 command:disconnect(Res, _) :- sendResponse(Res),log1(disconnect),log1('exit process').
 
 % DebugSession
@@ -92,13 +85,16 @@ thread_id(1).
 :- setDebuggerLinesStartAt1(true).
 :- setDebuggerColumnsStartAt1(true).
 
+% 初期化
 command:initialize(Res, _) :-
   sendEvent(initialized),(Body=Res.get(body); Body=_{}),
   sendResponse(Res.put(body,
     Body.put([supportsConfigurationDonecommand:true,
+              %supportsSetExpression:true,
+              supportsSetVariable:true,
               supportsEvaluateForHovers:true]))).
-%:- dispatch('{"command":"initialize","seq":1}').
 
+% ブレークポイントの設定
 command:setBreakpoints(Res, Args) :-
   Path = Args.source.path,
   catch(parseFile(Path,Codes),Error,(log1('error:~w\n',Error),false)),
@@ -109,30 +105,27 @@ command:setBreakpoints(Res, Args) :-
 command:setBreakpoints(Res, _) :-
   sendResponse(Res.put(body, _{breakpoints:[]})),!.
 setBreakpoints1(Codes,Line,_{verified:V2,line:L4,id:Id}) :-
-    convertClientLineToDebugger(Line,L1),
-    setBreakpoints2(L1,Codes,L2,V),
-    (V=false,last(Codes,L3:_:_) -> V2=true;V2=V,L3=L2),
-    convertDebuggerLineToClient(L3,L4),
-    nb_getval(breakpointId,Id),
-    Id1 is Id+1,nb_setval(breakpointId,Id1),!.
+  convertClientLineToDebugger(Line,L1),
+  setBreakpoints2(L1,Codes,L2,V),
+  (V=false,last(Codes,L3:_:_) -> V2=true;V2=V,L3=L2),
+  convertDebuggerLineToClient(L3,L4),
+  nb_getval(breakpointId,Id),Id1 is Id+1,nb_setval(breakpointId,Id1),!.
 setBreakpoints2(L,[],L,false).
 setBreakpoints2(L,[L2:_:_|_],L2,true) :- L2 >= L,!.
 setBreakpoints2(L,[_|Cs],L2,V) :- setBreakpoints2(L,Cs,L2,V).
 last([A],A).
 last([_|As],A) :- last(As,A).
-%:- dispatch('{"command":"setBreakpoints","seq":1,"arguments":{"source":{"path":"../pasm.txt"},"lines":[1,2,3]}}').
 
+% スレッドの取得
 command:threads(Res,_) :- thread_id(Id),
   sendResponse(Res.put(body,_{threads:[_{id:Id, name:'thread 1'}]})).
 
+% デバッガ起動時の最初の実行
 command:launch(Res, Args) :-
-  nb_setval(sourceFile,Args.program),
-  vm:loadFile(Args.program),
+  nb_setval(sourceFile,Args.program),vm:loadFile(Args.program),
   (true=Args.get(stopOnEntry)->sendStopped(Res,entry)
   ; ( hitBreakPoint(true) -> sendStopped(Res,breakpoint)
-    ; thread_id(Id),command:continue(Res, _{threadId:Id})
-    )
-  ).
+    ; thread_id(Id),command:continue(Res, _{threadId:Id}))).
 
 % ▶ ボタンを押した時に呼ばれる
 command:continue(Res, _) :- \+vm:step,!,sendTerminated(Res).
@@ -145,56 +138,46 @@ sendStopped(Res, Reason) :- sendResponse(Res),thread_id(Id),
 hitBreakPoint(R) :-
   % 対象のファイルのブレークポイントを取得する
   nb_getval(sourceFile,File),breakPoints(File,Breakpoints),
-  vm:getLine(Line),
   % ブレークポイントがあれば止める
-  include(hitBreakPointFilter(Line),Breakpoints,Bps),
-  log1(line:Line;bps:Bps;Breakpoints),
+  vm:getLine(Line),include(hitBreakPointFilter(Line),Breakpoints,Bps),
   (Bps=[]->R=false;R=true),!.
 hitBreakPoint(false).
-hitBreakPointFilter(Line,Bp) :-
-  convertDebuggerLineToClient(Line,Bp.line).
-convertDebuggerPathToClient(P,P).
+hitBreakPointFilter(Line,Bp) :- convertDebuggerLineToClient(Line,Bp.line).
 
 % スタックトレース
 command:stackTrace(Res, Args) :-
-  nb_getval(sourceFile,SourceFile),
+  nb_getval(sourceFile,SourceFile),file_base_name(SourceFile,Name),
   convertDebuggerPathToClient(SourceFile,Path),
-  file_base_name(SourceFile,Name),
   Source=_{name:Name,path:Path,sourceReference:0},
   vm:frames(Frames),length(Frames,L),vm:getCode0(Code),
   foldl(stackTrace1(Source),Frames,(L,[],Code),(_,Frames2,_)),!,
-  length(Frames2,Count),
   (Start  = Args.get(startFrame) ; Start = 0),
-  (Levels = Args.get(levels) ; Levels = Count),
-  End is min(Count, Start+Levels)-1,
-  findall(F,(between(Start,End,I),nth0(I,Frames2,F)),Fs),
-  reverse(Fs,RFs),
-  sendResponse(Res.put(body,_{stackFrames: RFs,totalFrames: Count})).
-
+  (Levels = Args.get(levels) ; Levels = L),
+  End is min(L, Start+Levels)-1,
+  findall(F,(between(Start,End,I),nth0(I,Frames2,F)),Fs),reverse(Fs,RFs),
+  sendResponse(Res.put(body,_{stackFrames: RFs,totalFrames: L})).
 stackTrace1(Source,Frame,(Id,Frames1,(Line1:_:_)),(Id1,[R|Frames1],Code2)) :-
   convertDebuggerLineToClient(Line1,Line),
   R = _{id:Id,name: Frame.nm,source:Source,line:Line, column:0},
   vm:getCode(Frame.pos,Code2),Id1 is Id-1.
 
-sendTerminated(Res) :- sendResponse(Res), sendEvent(terminated).
-
 % ステップオーバー
-command:next(Res, Args) :-
-  vm:frames(Frames),length(Frames,Len),!,next(Res,Args,Len).
+command:next(Res, Args) :- vm:frameLength(L),!,next(Res,Args,L).
 next(Res,_,_) :- \+vm:step,!, sendTerminated(Res).
 next(Res,_,_) :- hitBreakPoint(true),!, sendStopped(Res,breakpoint).
-next(Res,_,Len) :- vm:frames(Frames),length(Frames,L),Len >= L, !,
-                   sendStopped(Res,step).
+next(Res,_,Len) :- vm:frameLength(L),Len >= L, !,sendStopped(Res,step).
 next(Res,Args,Len) :- next(Res,Args,Len).
-
+% ステップイン
 command:stepIn(Res, _) :- \+vm:step,!, sendTerminated(Res).
 command:stepIn(Res, _) :- sendStopped(Res,step).
-command:stepOut(Res, Args) :- vm:frames(Fs),length(Fs,Len), stepOut(Res,Args,Len).
+% ステップアウト
+command:stepOut(Res, Args) :- vm:frameLength(L), stepOut(Res,Args,L).
 stepOut(Res,_,_) :- \+vm:step,!, sendTerminated(Res).
 stepOut(Res,_,_) :- hitBreakPoint(true),!, sendStopped(Res,breakpoint).
-stepOut(Res,_,Len) :- vm:frames(Fs),length(Fs,L),L < Len,!, sendStopped(Res,step).
+stepOut(Res,_,Len) :- vm:frameLength(L),L < Len,!, sendStopped(Res,step).
 stepOut(Res,Args,Len) :- stepOut(Res,Args,Len).
 
+% 変数のスコープ
 command:scopes(Res, Args) :-
   concat(global_,Args.frameId,FrameRef),createHandler(FrameRef,Ref),
   Scopes =[_{name:'Local', variablesReference:Ref, expensive:false}],
@@ -204,18 +187,21 @@ command:scopes(Res, Args) :-
 createHandler(V,HId) :-
   nb_getval(handeler_id,HId),HId1 is HId+1,nb_setval(handeler_id,HId1),
   assert(variableHandles(HId = V)).
+% 変数の値の連想配列取得
 command:variables(Res, Args) :-
   variableHandles(Args.variablesReference=_),
   nb_getval(vars,Vars),dict_pairs(Vars,_,Pairs),
-  findall(_{name:I,type:integer,value:A,variablesReference:0},(
+  findall(_{name:I,type:integer,value:A,variablesReference:0,storageItem:A},(
     member(I-V,Pairs),format(atom(A),'~w',[V])
   ),Vs),
   sendResponse(Res.put(body,_{variables:Vs})).
 command:variables(Res,_) :- sendResponse(Res.put(body,_{variables:[]})).
+% 変数設定
 command:setVariable(Res, Args) :-
-  atom_number(Args.value,N),vm:setValue(Args.name, N),
+  atom_number(Args.value,N),vm:set(Args.name, N),
   format(atom(A),'~w',[N]),
   sendResponse(Res.put(body,_{value:A,variablesReference: 0})).
+% 変数名などから計算
 command:evaluate(Res, Args) :-
   E1=Args.expression,
   vm:parseImm(E1,E),nb_getval(vars,Vars),
--- dist/prolog/vm.pl
+++ dist/prolog/vm.pl
@@ -1,9 +1,9 @@
 :- module(vm,[step/1]).
 parseImm(Src,Term) :- read_term_from_atom(Src,Term,[]).
-
 frame(Label,P) :- nb_getval(vars,Vars),nb_getval(currentPos,Pos),
                   asserta(frame(_{p:P,vars:Vars,pos:Pos,nm:Label})).
 frames(Frames) :- findall(Frame,frame(Frame),Frames).
+frameLength(Len) :- frames(Fs),length(Fs,Len).
 loadFile(Filename) :-
   retractall(label(_)),retractall(code(_)),retractall(frame(_)),
   parse(Filename,Funs),loadFun(0,Funs),label(main=Pos),
@@ -13,37 +13,29 @@ loadFun(I,[_:_:A:-T|Fs]) :- assert(label(A=I)),loadTerm(T,I,I2),loadFun(I2,Fs).
 loadTerm((A,B)) --> loadTerm(A),loadTerm(B).
 loadTerm(T,I,I1) :- I1 is I+1, assert(code(I=T)).
 
-getValue_(I,I) :- integer(I),!.
-getValue_(A,V) :- atom(A),nb_getval(vars,Vars),V=Vars.get(A).
-getValue(I,V) :- getValue_(I,V),!.
-getValue(A,0) :- atom(A).
+get_(I,I) :- integer(I),!.
+get_(A,V) :- atom(A),nb_getval(vars,Vars),V=Vars.get(A).
+get(I,V) :- get_(I,V),!.
+get(A,0) :- atom(A).
 
 getLine(L) :- nb_getval(currentPos,P),code(P=L:_:_).
 getCode(P,C) :- code(P=C).
 getCode0(C) :- nb_getval(currentPos,P),code(P=C).
-setValue(Reg, V) :- nb_getval(vars,Vars),nb_setval(vars,Vars.put(Reg,V)).
+set(Reg, V) :- nb_getval(vars,Vars),nb_setval(vars,Vars.put(Reg,V)).
 next :- nb_getval(currentPos,P),P1 is P+1,nb_setval(currentPos,P1),code(P1=_).
 step :- nb_getval(currentPos,P), code(P=(_:_:Code)),step(Code).
-step(add(A,B,C)) :- getValue(A,I1),getValue(B,I2),I is I1+I2,setValue(C,I),next.
-step(sub(A,B,C)) :- getValue(A,I1),getValue(B,I2),I is I1-I2,setValue(C,I),next.
-step(mul(A,B,C)) :- getValue(A,I1),getValue(B,I2),I is I1*I2,setValue(C,I),next.
-step(div(A,B,C)) :- getValue(A,I1),getValue(B,I2),I is I1 div I2,setValue(C,I),next.
-step(print(A)) :- getValue(A,V),log([V]),next.
+step(add(A,B,C)) :- get(A,I1),get(B,I2),I is I1+I2,set(C,I),next.
+step(sub(A,B,C)) :- get(A,I1),get(B,I2),I is I1-I2,set(C,I),next.
+step(mul(A,B,C)) :- get(A,I1),get(B,I2),I is I1*I2,set(C,I),next.
+step(div(A,B,C)) :- get(A,I1),get(B,I2),I is I1 div I2,set(C,I),next.
+step(print(A)) :- get(A,V),log([V]),next.
 step(ret(A)) :-
-    frames(Frames),length(Frames,Len),Len>1 ->
-      retract(frame(F)),frame(_),
-      getValue(A,V),NF = F.put(vars,F.vars.put(F.p, V)),
-      nb_setval(vars,NF.vars),
-      nb_setval(currentPos,F.pos),
-      next
-    ; findall(_,code(_=_),L),length(L,Len2),nb_setval(currentPos,Len2),!,false.
+  frames(Frames),length(Frames,Len),Len>1 ->
+    retract(frame(F)),get(A,V),NF=F.put(vars,F.vars.put(F.p, V)),
+    nb_setval(vars,NF.vars),nb_setval(currentPos,F.pos),next
+  ; findall(_,code(_=_),L),length(L,Len2),nb_setval(currentPos,Len2),!,false.
 step(call(Label,Params,P)) :-
-      label(Label=Pos),code(Pos=_:_:C),
-      enter(Enter)=C,
-      frame(Label,P),
-      maplist(getValue,Params,Ps2),
-      maplist([A,B,A=B]>>!,Enter,Ps2,Vars1),
-      Vars = _{}.put(Vars1),
-      nb_setval(vars, Vars),nb_setval(currentPos, Pos),
-      next.
+  label(Label=Pos),code(Pos=_:_:C),enter(Enter)=C,frame(Label,P),
+  maplist(get,Params,Ps2),maplist([A,B,A=B]>>!,Enter,Ps2,Vars1),
+  Vars = _{}.put(Vars1),nb_setval(vars, Vars),nb_setval(currentPos, Pos),next.
 step(call(Label,_,_)) :- error("not found label ~w", [Label]).
